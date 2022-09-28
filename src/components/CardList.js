14d6a6d8ff7076ee07002c80de652179af393623
Guilherme Alves <guilherme@mgesportes.com.br>
2022-09-28 14:22:26 -0300

    Requisito 10

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -3,35 +3,58 @@ import React from 'react';
 import Card from './Card';
 
 class CardList extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      searchValue: '',
+    };
+  }
+
+  handleSearchValue = (event) => {
+    this.setState({ searchValue: event.target.value.toLowerCase() });
+  };
+
   render() {
+    const { searchValue } = this.state;
     const { savedCards, handleRemove } = this.props;
     const cardList = savedCards;
 
     return (
       <div>
         <h1>Meu Baralho</h1>
-        {cardList.map((actualCard, index) => (
-          <div key={ index }>
-            <Card
-              key={ actualCard.cardName }
-              cardName={ actualCard.cardName }
-              cardDescription={ actualCard.cardDescription }
-              cardAttr1={ actualCard.cardAttr1 }
-              cardAttr2={ actualCard.cardAttr2 }
-              cardAttr3={ actualCard.cardAttr3 }
-              cardImage={ actualCard.cardImage }
-              cardRare={ actualCard.cardRare }
-              cardTrunfo={ actualCard.cardTrunfo }
-            />
-            <button
-              data-testid="delete-button"
-              type="button"
-              onClick={ handleRemove }
-            >
-              Remover Carta
-            </button>
-          </div>
-        ))}
+        <input
+          onChange={ this.handleSearchValue }
+          type="text"
+          name="search"
+          id="search"
+          value={ searchValue }
+          data-testid="name-filter"
+          placeholder="Digite aqui o nome de uma carta"
+        />
+        {cardList.filter((actualCard) => actualCard.cardName.toLowerCase()
+          .includes(searchValue))
+          .map((actualCard, index) => (
+            <div key={ index }>
+              <Card
+                key={ actualCard.cardName }
+                cardName={ actualCard.cardName }
+                cardDescription={ actualCard.cardDescription }
+                cardAttr1={ actualCard.cardAttr1 }
+                cardAttr2={ actualCard.cardAttr2 }
+                cardAttr3={ actualCard.cardAttr3 }
+                cardImage={ actualCard.cardImage }
+                cardRare={ actualCard.cardRare }
+                cardTrunfo={ actualCard.cardTrunfo }
+              />
+              <button
+                data-testid="delete-button"
+                type="button"
+                onClick={ handleRemove }
+              >
+                Remover Carta
+              </button>
+            </div>
+          ))}
       </div>
     );
   }
